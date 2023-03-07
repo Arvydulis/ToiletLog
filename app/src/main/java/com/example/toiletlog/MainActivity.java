53a522b688da736f2baf0f546f9c869dcee3320c
Arvydas <arvydas.mazauskas@ktu.edu>
2023-03-07 15:49:00 +0200

    Main menu activity

--- app/src/main/java/com/example/toiletlog/MainActivity.java
+++ app/src/main/java/com/example/toiletlog/MainActivity.java
@@ -3,12 +3,49 @@ package com.example.toiletlog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button addBtn, listBtn, settingsBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_menu_activity);
+
+        addBtn = findViewById(R.id.btn_addNew);
+        listBtn = findViewById(R.id.btn_list);
+        settingsBtn = findViewById(R.id.btn_settings);
+
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show add entry activity
+                Toast.makeText(getApplicationContext(), "add entry", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        listBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show list activity
+                Toast.makeText(getApplicationContext(), "show list", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // show settings activity
+                Toast.makeText(getApplicationContext(), "settings", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+
 }
