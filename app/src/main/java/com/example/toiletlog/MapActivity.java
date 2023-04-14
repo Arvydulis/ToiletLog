5a8fd265297e14e6c2a3342952543dab9df63b1c
Arvydas <arvydas.mazauskas@ktu.edu>
2023-04-14 21:45:09 +0300

    Current location + permisions + address conversion

--- app/src/main/java/com/example/toiletlog/MapActivity.java
+++ app/src/main/java/com/example/toiletlog/MapActivity.java
@@ -5,18 +5,24 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.preference.PreferenceManager;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.drawable.Drawable;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -24,6 +30,12 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.Priority;
 import com.google.android.gms.location.places.Place;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,6 +48,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -43,8 +56,10 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
@@ -54,23 +69,47 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     ActionBarDrawerToggle drawerToggle;
     Navbar navbar = new Navbar();
 
+    TextView addressLabel;
+
     DatabaseReference db_ref;
     List<LocationData> locations = new ArrayList<>();
 
     final String KAUNAS_ID = "ChIJQ9NnsXAi50YRvIs3x-DRS2E";
 
+    FusedLocationProviderClient fusedLocationProviderClient;
+    GoogleMap map;
+    LatLng kaunas = new LatLng(54.898521, 23.903597);
+    final static int REQUEST_CODE = 100;
+
+    boolean isUpdating;
+    LocationRequest locationRequest;
+    LocationCallback locationCallBack;
+
+    Geocoder geocoder;
+    List<Address> address;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
 
+        addressLabel = findViewById(R.id.bp_address);
+
         //InstantiateAppBarAndNav();
         navbar.InstantiateAppBarAndNav(this, R.string.title_map);
-;
+        ;
         db_ref = FirebaseDatabase.getInstance().getReference("Locations");
 //        LocationData vilnius = new LocationData("Vilnius", 54.687978, 25.278062);
 //        db_ref.child(vilnius.getTitle()).setValue(vilnius);
+
+        locationRequest = new LocationRequest.Builder(Priority.PRIORITY_BALANCED_POWER_ACCURACY, 100)
+                .setWaitForAccurateLocation(false)
+                .setMinUpdateIntervalMillis(2000)
+                .setMaxUpdateDelayMillis(1000)
+                .build();
+
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
@@ -79,14 +118,25 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     }
 
 
-
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
+        map = googleMap;
         googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(this, R.raw.custom_map_style));
-        LatLng kaunas = new LatLng(54.898521, 23.903597);
-//        googleMap.addMarker(new MarkerOptions().position(kaunas).title("Marker in Kaunas"));
-        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(kaunas, 10.0f));
 
+//        googleMap.addMarker(new MarkerOptions().position(kaunas).title("Marker in Kaunas"));
+        AskPermission();
+
+        locationCallBack = new LocationCallback() {
+            @Override
+            public void onLocationResult(@NonNull LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+
+                Location location = locationResult.getLastLocation();
+                if (location != null) {
+                    UpdateLocation(location);
+                }
+            }
+        };
 
         //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 12.0f));
 
@@ -95,7 +145,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         db_ref.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                if (snapshot.exists()){
+                if (snapshot.exists()) {
                     LocationData data = snapshot.getValue(LocationData.class);
                     locations.add(data);
                     LatLng location = new LatLng(data.getLatitude(), data.getLongitude());
@@ -140,6 +190,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
             @Override
             public boolean onMarkerClick(@NonNull Marker marker) {
                 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(marker.getPosition(), 18.0f));
+                ShowLocationAddress(marker.getPosition());
                 return true;
             }
         });
@@ -153,7 +204,7 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)){
+        if (drawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
 
@@ -170,11 +221,21 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public void onBackPressed() {
-        if(navbar.drawerLayout.isDrawerOpen(GravityCompat.START)){
+        if (navbar.drawerLayout.isDrawerOpen(GravityCompat.START)) {
             navbar.drawerLayout.closeDrawer(GravityCompat.START);
-        }else {
+        } else {
             super.onBackPressed();
         }
+    }
+
+    @SuppressLint("MissingPermission")
+    void StartLocationUpdates() {
+
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest, locationCallBack, null);
+    }
+
+    void StopLocationUpdates() {
+        fusedLocationProviderClient.removeLocationUpdates(locationCallBack);
     }
 
     private BitmapDescriptor BitmapFromVector(Context context, int vectorResId) {
@@ -200,5 +261,81 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
         // after generating our bitmap we are returning our bitmap.
         return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
+    void GetLastLocation() {
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationProviderClient.getLastLocation().addOnSuccessListener(new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+
+                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+//                        MarkerOptions markerOptions = new MarkerOptions()
+//                                .position(latLng)
+//                                .title("User location")
+//                                .icon(BitmapDescriptorFactory.);
+//                        //.icon(BitmapFromVector(getApplicationContext(), R.drawable.baseline_location_on_24));
+//                        Marker marker = map.addMarker(markerOptions);
+                        map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 17.0f));
+
+                        //StartLocationUpdates();
+                    }
+                }
+            });
+            //fusedLocationProviderClient.requestLocationUpdates()
+        }
+
+    }
+
+    void ShowLocationAddress(LatLng location){
+        String chosenAddress = GetAddress(location);
+        //Message.ShowToast(getApplicationContext(), chosenAddress);
+        addressLabel.setText(chosenAddress);
+    }
+
+    String GetAddress(LatLng location){
+        try {
+            address = geocoder.getFromLocation(location.latitude, location.longitude, 1);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return address.get(0).getAddressLine(0);
+    }
+
+    void UpdateLocation(Location location) {
+        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+        map.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+    }
+
+    void AskPermission() {
+        ActivityCompat.requestPermissions(MapActivity.this,
+                new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    return;
+                }
+                map.setMyLocationEnabled(true);
+
+                geocoder = new Geocoder(MapActivity.this, Locale.getDefault());
+
+                GetLastLocation();
+            }
+            else {
+                map.animateCamera(CameraUpdateFactory.newLatLngZoom(kaunas, 10.0f));
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
     }
 }
