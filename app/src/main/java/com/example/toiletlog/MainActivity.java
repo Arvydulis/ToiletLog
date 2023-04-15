6404026d37b8049fcc21721cb0cc93f53b0cac60
Armandas <99257853+Amaradu@users.noreply.github.com>
2023-04-15 17:27:38 +0300

    Notif timer done
    Works normal on emulator, dosent work on xiomi note 11s

--- app/src/main/java/com/example/toiletlog/MainActivity.java
+++ app/src/main/java/com/example/toiletlog/MainActivity.java
@@ -59,12 +59,12 @@ public class MainActivity extends AppCompatActivity {
         AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
 
         Intent intent = new Intent(this, NotificationReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, 0);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_MUTABLE);
 
         Calendar calendar = Calendar.getInstance();
         calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.set(Calendar.HOUR_OF_DAY, 16);
-        calendar.set(Calendar.MINUTE, 36);
+        calendar.set(Calendar.HOUR_OF_DAY, 17);
+        calendar.set(Calendar.MINUTE, 15);
 
         alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pendingIntent);
 
--- app/src/main/java/com/example/toiletlog/NotificationReceiver.java
+++ app/src/main/java/com/example/toiletlog/NotificationReceiver.java
@@ -4,38 +4,66 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
+import android.os.Handler;
 
 import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
 public class NotificationReceiver extends BroadcastReceiver {
-
+    private static final int NOTIFICATION_ID = 1; // Unique ID for the notification
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        Handler handler = new Handler(); // Create a new handler for running code on the main thread
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // Do some long-running operation here (e.g. fetch data from server)
+                final String data = fetchData();
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Daily")
-                .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("ToiletLog")
-                .setContentText("Did you poop?")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                // Set the intent that will fire when the user taps the notification
-                .setAutoCancel(true);
+                // Display the notification on the main thread
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Create the notification
+                        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, "Daily")
+                                .setSmallIcon(R.drawable.baseline_list_24)
+                                .setContentTitle("ToiletLog")
+                                .setContentText("Did you poop today?")
+                                .setPriority(NotificationCompat.PRIORITY_HIGH)
+                                .setAutoCancel(true);
 
-        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+                        // Display the notification
+                        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
 
-        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
+
+                        if (ActivityCompat.checkSelfPermission(context, android.Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                            // TODO: Consider calling
+                            //    ActivityCompat#requestPermissions
+                            // here to request the missing permissions, and then overriding
+                            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                            //                                          int[] grantResults)
+                            // to handle the case where the user grants the permission. See the documentation
+                            // for ActivityCompat#requestPermissions for more details.
+                            return;
+                        }
+                        notificationManager.notify(NOTIFICATION_ID, builder.build());
+                    }
+                });
+            }
+        }).start();
+    }
+
+    private String fetchData() {
+        // Perform some long-running operation here (e.g. fetch data from server)
+        try {
+            Thread.sleep(5000); // Simulate delay for demonstration purposes
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
-        managerCompat.notify(1, builder.build());
-
+        return "Some data";
     }
 }
+
